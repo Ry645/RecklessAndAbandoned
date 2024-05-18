40167b4403595f22b06fbf38d8375b6a3e25c396
Ryan Sexton <ry645.dev@gmail.com>
2024-05-17 20:23:13 -0400

    added vector conversions to function library (flib)

--- FunctionLibrary/functionLibrary.gd
+++ FunctionLibrary/functionLibrary.gd
@@ -1,1 +1,7 @@
-extends Node
+class_name FunctionLibrary
+
+static func vec2ToVec3(vector:Vector2, yValue = 0) -> Vector3:
+	return Vector3(vector.x, yValue, vector.y)
+
+static func vec3ToVec2(vector:Vector3) -> Vector2:
+	return Vector2(vector.x, vector.z)
