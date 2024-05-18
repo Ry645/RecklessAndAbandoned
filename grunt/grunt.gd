88f8c6e338013abc8523ce3068b0ff11ac7e6a41
Ryan Sexton <ry645.dev@gmail.com>
2024-05-18 01:44:22 -0400

    yeah the grunt ai is looking pretty good so far; I'll try to merge with master now

--- grunt/grunt.gd
+++ grunt/grunt.gd
@@ -9,7 +9,8 @@ class_name Grunt
 @export var backpedalSpeed:float = 6.0
 
 
-@export var surroundCircleRadius:float = 5.0
+@export var surroundCircleRadius:float = 10.0
+@export var backpedalCircleRadius:float = 5.0
 @export var maxStrafeTargetDeviation:float = 1.0
 @export var acceptanceRadiusDefault:float = 0.1
 
@@ -24,7 +25,6 @@ signal damagePlayer(damage)
 
 @onready var attack_interval_timer = %attackIntervalTimer
 @onready var attack_startup_timer = %attackStartupTimer
-@onready var attack_cooldown_timer = %attackCooldownTimer
 @onready var mesh_instance_3d = %MeshInstance3D
 
 #TEST
@@ -93,7 +93,11 @@ func _physics_process(delta):
 			emit_signal("targetTaken")
 			currentAiState = aiState.APPROACH
 		aiState.COMBAT:
-			pass
+			if attack_interval_timer.is_stopped():
+				attack_interval_timer.start()
+			strafeAround()
+			if FunctionLibrary.vec3ToVec2(pointToStrafeAround - targetBody.global_position).length() >= maxStrafeTargetDeviation:
+				currentAiState = aiState.BACKPEDAL
 		aiState.APPROACH:
 			var reachedDestination:bool = moveTo(targetBody.global_position, surroundSpeed, 2.0)
 			if reachedDestination:
@@ -103,12 +107,16 @@ func _physics_process(delta):
 			if attack_startup_timer.is_stopped():
 				attack_startup_timer.start()
 		aiState.BACKPEDAL:
-			currentAiState = aiState.LOSE_TARGET
+			var circlePos = get_circle_position(backpedalCircleRadius)
+			pointToStrafeAround = targetBody.global_position
+			var reachedDestination:bool = moveTo(Vector3(circlePos.x, 0, circlePos.y), backpedalSpeed)
+			if reachedDestination:
+				currentAiState = aiState.COMBAT
 		aiState.LOSE_TARGET:
 			emit_signal("targetFreed")
 			currentAiState = aiState.SURROUND
 		aiState.FORGET:
-			pass
+			currentAiState = aiState.WANDER
 		
 
 func applyGravity(delta):
@@ -171,15 +179,9 @@ func get_circle_position(circleRadius:float) -> Vector2:
 #TEST
 func _on_attack_interval_timer_timeout():
 	currentAiState = aiState.APPROACH
-	attack_startup_timer.start()
 
 #TEST
 func _on_attack_startup_timer_timeout():
 	currentAiState = aiState.BACKPEDAL
 	emit_signal("damagePlayer", 20)
 	#attack_cooldown_timer.start()
-
-#TEST
-func _on_attack_cooldown_timer_timeout():
-	currentAiState = aiState.SURROUND
-	attack_interval_timer.start()
