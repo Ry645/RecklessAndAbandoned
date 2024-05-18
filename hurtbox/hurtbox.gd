6a61442087b6cda994f3ad9ebd6472659e967a47
Ryan Sexton <ry645.dev@gmail.com>
2024-05-18 02:31:31 -0400

    working on hurtbox still, using shapecast3d instead, I'm going to bed

--- hurtbox/hurtbox.gd
+++ hurtbox/hurtbox.gd
@@ -1,18 +1,20 @@
-extends Area3D
+extends ShapeCast3D
 
 class_name Hurtbox
 
 #TODO
 #later to damage res
 @export var damageValue:float
+@export var activeFrames:int = 1
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	print(get_overlapping_bodies())
-	for body in get_overlapping_bodies():
+func _process(delta):
+	for body in whatever:
 		if body.has_method("takeDamage"):
 			body.takeDamage(damageValue)
 	
-	queue_free()
+	if activeFrames <= 0:
+		queue_free()
+	activeFrames -= 1
 
 
