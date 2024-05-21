99dacfd2888428788989919f0504ef63536c4cbe
Ryan Sexton <ry645.dev@gmail.com>
2024-05-21 14:51:23 -0400

    finished working on health bar

--- healthBar/health_bar.gd
+++ healthBar/health_bar.gd
@@ -1,13 +1,15 @@
 extends ProgressBar
 
-
+@onready var healthText = %healthText
 
 
 func _on_player_health_update(health):
 	value = health
+	healthText = %healthText # needed b/c onready order is off
+	healthText.text = str(health)
 
 
 func _on_player_set_health_bar_vars(minHealth, maxHealth, currentHealth):
 	min_value = minHealth
 	max_value = maxHealth
-	value = currentHealth
+	_on_player_health_update(currentHealth)
